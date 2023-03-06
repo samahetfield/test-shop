bf6a448f92abbdcc439d1c20a476bfa089bb1237
Sergio Samaniego <sergillo1996@hotmail.com>
2023-03-06 19:14:10 +0100

    Created project structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Header } from './Header/Header'
+import { ProductList } from './ProductList/ProductList';
+import { ProductDetail } from './ProductDetail/ProductDetail';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProductList />
+  },
+  {
+    path: "/product/:id",
+    element: <ProductDetail />
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <RouterProvider router={router} />
     </div>
   );
 }

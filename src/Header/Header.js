1a2fd6fff859ed2c131fafb25235573d8a9fd6a7
Sergio Samaniego <sergillo1996@hotmail.com>
2023-03-09 22:17:44 +0100

    Added some styles

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,17 +3,17 @@ import Badge from '@mui/material/Badge';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import imageLogo from '../images/logo.png';
 
 export default function Header({ shoppingCart }) {
   return (
     <div className="header">
       <Link to="/">
-        <h1>Test-shop</h1>
+        <img src={imageLogo} alt="Test shop" />
       </Link>
       <Badge badgeContent={shoppingCart} color="primary">
         <ShoppingCartIcon color="action" />
       </Badge>
     </div>
-    // https://www.youtube.com/watch?v=zy8rqihtvA8
   );
 }
